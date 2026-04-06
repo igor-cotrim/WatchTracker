896ca59677f28dab853da05e05ae8107a18b4e2b
igor-cotrim <igorcotrim.dev@gmail.com>
2026-04-06 14:47:36 -0300

    Initial Commit

---
+++ WatchTracker/WatchTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTrackerApp.swift
+//  WatchTracker
+//
+//  Created by Igor Cotrim on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
